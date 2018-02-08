56a7723f08b869cbe4e613183367c821d9e1d749
Feni Varughese <feni727@gmail.com>
2018-02-07 21:14:54 -0600

    Add a private config page to verify debug is false in prod

--- arevel/urls.py
+++ arevel/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from workspace.views import index
+from workspace.views import index, config
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^private/config$', config),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- workspace/views.py
+++ workspace/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.conf import settings
 
 def index(request):
     return HttpResponse("Hello, Arevel.")
+
+
+def config(request):
+    return JsonResponse({
+        "is_prod": settings.IS_PROD,
+        "debug": settings.DEBUG
+    })
