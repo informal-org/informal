05c3571e18a35810c91583e8941a0fb0f9379e8b
Feni Varughese <feni727@gmail.com>
2018-03-05 22:58:51 -0600

    Fix google cloud logging? I think...

--- workspace/views.py
+++ workspace/views.py
@@ -2,13 +2,17 @@ from django.http import HttpResponse, JsonResponse
 from django.conf import settings
 from django.shortcuts import render
 
+import logging
+
 
 def index(request):
+    logging.info("Homepage req")
     # return HttpResponse("Hello, Arevel.")
     return render(request, "index.html")
 
 
 def config(request):
+    logging.info("Config req")
     return JsonResponse({
         "is_prod": settings.IS_PROD,
         "debug": settings.DEBUG
