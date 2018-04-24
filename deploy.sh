af6766042f7f0c05dd54d16ff49aaa66b9b5cd98
Feni Varughese <feni727@gmail.com>
2018-04-24 08:42:24 -0500

    Fix static file permissions on google cloud storage

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 echo "Compiling npm packages for production."
 export NODE_ENV=production
 npm run build
-echo "Building python packages."
-workon arevel
-pip install -t dist/lib -r requirements.txt --force --upgrade
+#echo "Building python packages."
+#workon arevel
+#pip install -t dist/lib -r requirements.txt --force --upgrade
 echo "Collecting static files to ship to CDN"
 python manage.py collectstatic
 export CLOUDSDK_PYTHON="/usr/bin/python"
@@ -12,3 +12,4 @@ gcloud app deploy
 
 gsutil acl ch -u AllUsers:R gs://arevel-0.appspot.com
 gsutil rsync -R dist/static/ gs://arevel-0.appspot.com/static/
+gsutil -m acl ch -r -u AllUsers:R gs://arevel-0.appspot.com/static/
