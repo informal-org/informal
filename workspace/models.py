7dac0145a7472bac8c873d9b544d2d3b54c17eb6
Feni Varughese <feni727@gmail.com>
2018-07-04 12:18:22 -0500

    Beta signup and some landing page wording changes

--- workspace/models.py
+++ workspace/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+import uuid
 
 
 class Docs(models.Model):
     name = models.CharField(max_length=200)
     created_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
+
+
+class BetaSignup(models.Model):
+    email = models.EmailField()
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, db_index=True)
