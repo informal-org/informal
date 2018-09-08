871179764052952133dbfae01436439f17ecbccc
Feni Varughese <feni727@gmail.com>
2018-09-08 12:57:09 -0500

    Setting up draft and megadraft

--- static/app/app.js
+++ static/app/app.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Editor, EditorState, RichUtils} from 'draft-js';
 
+import {MegadraftEditor, editorStateFromRaw, editorStateToJSON} from "megadraft";
+
+
 
 class AvEditor extends React.Component {
     constructor(props) {
@@ -10,6 +13,7 @@ class AvEditor extends React.Component {
         this.onChange = (editorState) => this.setState({editorState});
         this.handleKeyCommand = this.handleKeyCommand.bind(this);
     }
+
     handleKeyCommand(command, editorState) {
         // Apply bold, italics, etc.
         const newState = RichUtils.handleKeyCommand(editorState, command);
@@ -19,6 +23,7 @@ class AvEditor extends React.Component {
         }
         return 'not-handled';
     }
+
     render() {
         return (
             <Editor editorState={this.state.editorState}
@@ -28,7 +33,66 @@ class AvEditor extends React.Component {
     }
 }
 
+
+class ArevelApp extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {editorState: editorStateFromRaw(null)};
+
+    const content = {
+      "entityMap": {},
+      "blocks": [
+        {
+          "key": "ag6qs",
+          "text": "Hello world",
+          "type": "unstyled",
+          "depth": 0,
+          "inlineStyleRanges": [],
+          "entityRanges": [],
+          "data": {}
+        }
+      ]
+    };
+
+    const editorState = editorStateFromRaw(content);
+    this.state = {editorState};
+
+
+  }
+
+  onChange = (editorState) => {
+    this.setState({editorState});
+  };
+
+  onSaveClick = () => {
+    const {editorState} = this.state;
+    const content = editorStateToJSON(editorState);
+    // Your function to save the content
+    // save_my_content(content);
+    console.log(content);
+  };
+
+
+  render() {
+    return (
+        <div>
+      <MegadraftEditor
+        editorState={this.state.editorState}
+        onChange={this.onChange}/>
+
+
+            <button onClick={this.onSaveClick}>
+                Save
+            </button>
+
+        </div>
+    )
+  }
+}
+
+
+
 let domContainer = document.querySelector('#root');
-ReactDOM.render(<AvEditor />, domContainer);
+ReactDOM.render(<ArevelApp />, domContainer);
 
 console.log("Hello world")
