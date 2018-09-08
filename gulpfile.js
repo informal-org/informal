879238b06abeba027621df2de6ded1c8d1e08bfb
Feni Varughese <feni727@gmail.com>
2018-09-08 07:53:35 -0500

    Setup react along with all of the webpack and other react bullshit

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,28 @@ var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
+const watchSass = require("gulp-watch-sass")
+
+const sassConfig = {
+    outputStyle: 'compressed'
+};
+
+const sassInput = [
+    'static/scss/*.scss'
+];
+const sassOutput = gulp.dest("static/css")
+
+
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['static/scss/*.scss'])
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }))
-        .pipe(gulp.dest("static/css"))
+    return gulp.src(sassInput)
+        .pipe(sass(sassConfig))
+        .pipe(sassOutput)
         .pipe(browserSync.stream());
 });
+
+gulp.task("sass:watch", () => watchSass(sassInput).pipe(sass(sassConfig))
+  .pipe(sassOutput));
+
+
+gulp.task('default', ['sass:watch']);
--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,23 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './static/ts/main.ts',
+  entry: './static/app/app.js',
   output: {
     path: path.resolve(__dirname, './static/js'),
     publicPath: '/static/js',
-    filename: 'arevel.js'
+    filename: 'app.js'
   },
   module: {
     rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-          }
-          // other vue-loader options go here
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
         }
-      },
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-        options: {
-          appendTsSuffixTo: [/\.vue$/],
-        }
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]'
-        }
-      },
-      {
-          test: /\.css$/,
-          use: [
-              'style-loader',
-              'css-loader'
-          ]
-      }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
-    alias: {
-      'vue$': 'vue/dist/vue.esm.js'
-    }
+      extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
