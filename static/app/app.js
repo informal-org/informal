8c4c562a1754fc0abd6c842a2f72eb22522b5351
Feni Varughese <feni727@gmail.com>
2018-09-08 09:12:11 -0500

    Setup draft js

--- static/app/app.js
+++ static/app/app.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
 
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+class AvEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
     }
-
-    return (
-      <button onClick={() => this.setState({ liked: true }) }>
-        Like
-      </button>
-    );
-  }
+    handleKeyCommand(command, editorState) {
+        // Apply bold, italics, etc.
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    }
+    render() {
+        return (
+            <Editor editorState={this.state.editorState}
+                    handleKeyCommand={this.handleKeyCommand}
+                    onChange={this.onChange} />
+        );
+    }
 }
 
 let domContainer = document.querySelector('#root');
-ReactDOM.render(<LikeButton />, domContainer);
+ReactDOM.render(<AvEditor />, domContainer);
 
 console.log("Hello world")
