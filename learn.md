bd6c5a1c429583bf161ce32fac0b7c8db2634ec8
Feni Varughese <feni727@gmail.com>
2019-04-14 16:55:49 -0500

    Enable web push

---
+++ learn.md
@@ -0,0 +1,3 @@
+use static_push instead of static_path for h2 push of assets.
+ets - key value store per instance. 
+pg2
--- lib/arevel_web/controllers/page_controller.ex
+++ lib/arevel_web/controllers/page_controller.ex
@@ -1,6 +1,9 @@
 defmodule ArevelWeb.PageController do
   use ArevelWeb, :controller
 
+  plug :push, "/js/app.js"
+  plug :push, "/css/app.css"
+
   def index(conn, _params) do
     render(conn, "index.html")
   end
