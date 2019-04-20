aab0472e8d203ab30fcd4bb2cfdc89e182739efb
Feni Varughese <feni727@gmail.com>
2019-04-19 20:08:51 -0500

    Deploy scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -21,6 +21,11 @@ gsutil cp _build/prod/rel/arevel/bin/arevel.run \
 
 echo "Restarting instaces"
 # Restart instances to pick up changes.
-gcloud compute instance-groups managed rolling-action start-update arevel-group
 
+# Doesn't seem to do anything??
+gcloud compute instance-groups managed rolling-action start-update arevel-group \
+    --version template=arevel-template --zone us-central1-b \
+    -type proactive
+
+gcloud compute instance-groups managed rolling-action replace arevel-group --zone us-central1-b
 # Be sure to verify that the deploy went through.
--- lib/arevel_web/controllers/page_controller.ex
+++ lib/arevel_web/controllers/page_controller.ex
@@ -14,6 +14,9 @@ end
 defmodule ArevelWeb.HealthCheckController do
   use ArevelWeb, :controller
 
+  # Currently configured to just look for any response.
+  # TODO: Status based and giving more output.
+  # TODO: Restrict to internal network?
   def healthcheck(conn, _params) do
     json(conn, %{status: "OK"})
   end
