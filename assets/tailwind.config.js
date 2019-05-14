f43b3071fab373363c0c2e41bb43d23adefa0b49
Feni Varughese <feni727@gmail.com>
2019-05-13 22:11:48 -0500

    Reorganize base tailwind size and got some of the skeleton of the base page in place.

---
+++ assets/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ["Source Sans Pro", "Helevetica", "Roboto", "Arial", 'sans-serif'],
+      body: ["Source Sans Pro", "Helevetica", "Roboto", "Arial", 'sans-serif'],
+      sans: ["Source Sans Pro", "Helevetica", "Roboto", "Arial", 'sans-serif'],
+    }
+  },
+  variants: {},
+  plugins: []
+}
