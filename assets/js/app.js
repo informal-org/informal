65bc4354593c9ed7f56dae0f9d6f3b1066425e61
Feni Varughese <feni727@gmail.com>
2019-05-24 21:38:28 -0500

    Javascript code reorganization and clean up. Split up parsing modules into a separate expr.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,68 +1,9 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
+// Import CSS so webpack loads it. MiniCssExtractPlugin will split it.
 import css from "../css/app.css"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
 import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
-
-import * as jsep from "jsep"
-
-// Override default jsep expressions with our language equivalents.
-// Semi case-insensitive. Done at the parser level so we get clean input.
-jsep.addBinaryOp("OR", 1);
-jsep.addBinaryOp("Or", 1);
-jsep.addBinaryOp("or", 1);
-
-jsep.addBinaryOp("AND", 2);
-jsep.addBinaryOp("And", 2);
-jsep.addBinaryOp("and", 2);
-
-jsep.addBinaryOp("MOD", 10);
-jsep.addBinaryOp("Mod", 10);
-jsep.addBinaryOp("mod", 10);
-
-jsep.addBinaryOp("IS", 6);  // 6 is Priority of ==
-jsep.addBinaryOp("Is", 6);
-jsep.addBinaryOp("is", 6);
-
-jsep.addUnaryOp("NOT");
-jsep.addUnaryOp("Not");
-jsep.addUnaryOp("not");
-
-// Remove un-supported operations - use verbal names for these instead. 
-jsep.removeBinaryOp("%");
-jsep.removeBinaryOp("||");
-jsep.removeBinaryOp("&&");
-jsep.removeBinaryOp("|");
-jsep.removeBinaryOp("&");
-jsep.removeBinaryOp("==");
-jsep.removeBinaryOp("!=");  // not (a = b). Excel does <>
-jsep.removeBinaryOp("===");
-jsep.removeBinaryOp("!==");
-jsep.removeBinaryOp("<<");
-jsep.removeBinaryOp(">>");
-jsep.removeBinaryOp(">>>");
-
-jsep.removeUnaryOp("!")
-jsep.removeUnaryOp("~")
-
-jsep.addLiteral("True", true)
-jsep.addLiteral("TRUE", true)
-
-jsep.addLiteral("False", false)
-jsep.addLiteral("FALSE", false)
-
+import "./expr.js"
+import React from "React";
+import ReactDOM from "react-dom";
 
 function postData(url = '', data = {}) {
     return fetch(url, {
@@ -80,98 +21,6 @@ function postData(url = '', data = {}) {
     })
 }
 
-// import LiveSocket from "phoenix_live_view"
-// import View from "phoenix_live_view"
-
-// let liveSocket = new LiveSocket("/live")
-// liveSocket.connect()
-
-// window.phxSocket = liveSocket;
-
-
-
-function getLiveView() {
-    // Get the first live view. This isn't available till after all the loading.
-    if(window.phxView === undefined){
-        window.phxView = window.phxSocket.getViewById(Object.keys(liveSocket.views)[0])
-    }
-    return window.phxView;
-}
-
-/*
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <h1>Hello, world!!</h1>,
-    document.getElementById('root')
-  );
-  
-
-class Cell extends React.Component {
-    render() {
-        return (
-            <div class="Cell">
-                <input id="expr-input" name="expression" value="<%= @expression %>"
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text"></input>                
-            </div>
-        )
-    }
-}
-
-*/
-
-function urlencode(dict) {
-    let params = new URLSearchParams();
-    return params.toString()
-}
-
-function parse_expr(expr) {
-    // console.log(event);
-    
-    // var expr = event.target.getElementsByClassName("expr-input")[0].value;
-    console.log(expr);
-    console.log(parsed);
-
-    var onReply = function(e){
-        console.log("Got response back")
-        console.log(e);
-    }
-
-    var encoded = JSON.stringify(parsed);
-    console.log("Encoded")
-    console.log(encoded);
-    
-    getLiveView().pushWithReply("event", {
-        type: "form",
-        event: "evaluate",
-        value: new URLSearchParams({
-            "id": cellId,
-            "input": expr,
-            "parsed": encoded
-        }).toString()
-      }, onReply)
-  
-    /*
-    */
-
-    return false;
-}
-
-
-import React from "React";
-import ReactDOM from "react-dom";
-
-
-(function(){    
-    console.log("my code 4")
-    // Should listen to the high level wrapper of this seciton.
-    // document.addEventListener("submit", parse_expr, true);
-})();
-
-console.log("latest version")
-
 const initialState = {
     "cells": [
         {
@@ -186,7 +35,6 @@ const initialState = {
 }
 
 var NEXT_ID = initialState["cells"].length + 1;
-
 
 class Cell extends React.Component {
     constructor(props) {
---
+++ assets/js/expr.js
@@ -0,0 +1,84 @@
+import * as jsep from "jsep"
+
+// Override default jsep expressions with our language equivalents.
+// Semi case-insensitive. Done at the parser level so we get clean input.
+jsep.addBinaryOp("OR", 1);
+jsep.addBinaryOp("Or", 1);
+jsep.addBinaryOp("or", 1);
+
+jsep.addBinaryOp("AND", 2);
+jsep.addBinaryOp("And", 2);
+jsep.addBinaryOp("and", 2);
+
+jsep.addBinaryOp("MOD", 10);
+jsep.addBinaryOp("Mod", 10);
+jsep.addBinaryOp("mod", 10);
+
+jsep.addBinaryOp("IS", 6);  // 6 is Priority of ==
+jsep.addBinaryOp("Is", 6);
+jsep.addBinaryOp("is", 6);
+
+jsep.addUnaryOp("NOT");
+jsep.addUnaryOp("Not");
+jsep.addUnaryOp("not");
+
+// Remove un-supported operations - use verbal names for these instead. 
+jsep.removeBinaryOp("%");
+jsep.removeBinaryOp("||");
+jsep.removeBinaryOp("&&");
+jsep.removeBinaryOp("|");
+jsep.removeBinaryOp("&");
+jsep.removeBinaryOp("==");
+jsep.removeBinaryOp("!=");  // not (a = b). Excel does <>
+jsep.removeBinaryOp("===");
+jsep.removeBinaryOp("!==");
+jsep.removeBinaryOp("<<");
+jsep.removeBinaryOp(">>");
+jsep.removeBinaryOp(">>>");
+
+jsep.removeUnaryOp("!")
+jsep.removeUnaryOp("~")
+
+jsep.addLiteral("True", true)
+jsep.addLiteral("TRUE", true)
+
+jsep.addLiteral("False", false)
+jsep.addLiteral("FALSE", false)
+
+
+function urlencode(dict) {
+    let params = new URLSearchParams();
+    return params.toString()
+}
+
+function parse_expr(expr) {
+    // console.log(event);
+    
+    // var expr = event.target.getElementsByClassName("expr-input")[0].value;
+    console.log(expr);
+    console.log(parsed);
+
+    var onReply = function(e){
+        console.log("Got response back")
+        console.log(e);
+    }
+
+    var encoded = JSON.stringify(parsed);
+    console.log("Encoded")
+    console.log(encoded);
+    
+    getLiveView().pushWithReply("event", {
+        type: "form",
+        event: "evaluate",
+        value: new URLSearchParams({
+            "id": cellId,
+            "input": expr,
+            "parsed": encoded
+        }).toString()
+      }, onReply)
+  
+    /*
+    */
+
+    return false;
+}
