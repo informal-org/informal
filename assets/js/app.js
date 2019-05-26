0325d7598bd0561069ae81ac52c4229ea8489b12
Feni Varughese <feni727@gmail.com>
2019-05-26 10:26:43 -0500

    Goodbye elixir and phoenix. I'll miss you.

--- assets/js/app.js
+++ assets/js/app.js
@@ -108,18 +108,12 @@ class GridCell extends React.Component {
         this.setState({name: event.target.value});
     }
     render() {
-        let cellStyle = {};
-        if(this.state.display.width > 1){
-            cellStyle["gridColumnEnd"] = "span " + this.state.display.width;
-        }
-        if(this.state.display.height > 1){
-            cellStyle["gridRowEnd"] = "span " + this.state.display.height;
-        }
         let className = "Cell";
+        className += " Cell--width" + this.state.display.width;
+        className += " Cell--height" + this.state.display.height;
         if(this.props.isFocused){
             className += " Cell--focused";
         }
-
         if(this.props.isError) {
             className += " Cell--error";
         }
@@ -129,7 +123,19 @@ class GridCell extends React.Component {
             cellBody = <form onSubmit={this.saveCell}>
             <input className="Cell-cellName block Cell-cellName--edit" placeholder="Name" type="text" onChange={this.changeName} value={this.state.name}></input> 
             <input className="Cell-cellValue bg-blue-100 block Cell-cellValue--edit" type="text" onChange={this.changeInput} value={this.state.input}></input>
-            <b className="Cell-cellResult">{this.state.output ? this.state.output : " "}</b>
+            <span className="Cell-cellResult inline-block">{this.state.output ? this.state.output : " "}
+            
+            
+            m Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, writ
+            
+            </span>
             <input type="submit" className="hidden"/>
           </form>
         } else {
@@ -139,7 +145,7 @@ class GridCell extends React.Component {
             </span>
         }
 
-        return <div className={className} style={cellStyle} onClick={this.setFocus}>
+        return <div className={className} onClick={this.setFocus}>
             {cellBody}
         </div>
     }
