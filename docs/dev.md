12f5f469ca6beaaf01d9935f1dec139210faecda
Feni Varughese <feni727@gmail.com>
2019-05-27 13:46:23 -0500

    Generate cell models and migrations.

---
+++ docs/dev.md
@@ -0,0 +1,24 @@
+Schema
+    Cell
+    - (user)
+#   - parent - cell
+    - pre_offset - int - Count of cells before this one that's "empty".
+    - previous - cell
+    - next - cell
+
+#   - children: list or reference to first.
+#   - head, tail - no need, can just search for previous = null or next = null.
+
+    - uuid
+    - input - string
+    - parsed - json
+    - output - json
+    - id - 
+    - dependencies - list of 
+
+Create ecto migrations
+mix phx.gen.schema Cell cells name:string uuid:uuid input:string parsed:map output:map dependencies:array:uuid pre_offset:integer previous:uuid next:uuid
+
+
+Apply migration
+mix ecto.migrate
---
+++ lib/arevel/cell.ex
@@ -0,0 +1,25 @@
+defmodule Arevel.Cell do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "cells" do
+    field :dependencies, {:array, Ecto.UUID}
+    field :input, :string
+    field :name, :string
+    field :next, Ecto.UUID
+    field :output, :map
+    field :parsed, :map
+    field :pre_offset, :integer
+    field :previous, Ecto.UUID
+    field :uuid, Ecto.UUID
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(cell, attrs) do
+    cell
+    |> cast(attrs, [:name, :uuid, :input, :parsed, :output, :dependencies, :pre_offset, :previous, :next])
+    |> validate_required([:name, :uuid, :input, :parsed, :output, :dependencies, :pre_offset, :previous, :next])
+  end
+end
