e8e31f39fd53678f16588fc35c349dbeebaafeee
Feni Varughese <feni727@gmail.com>
2019-07-04 02:46:57 -0500

    WIP more parser approach experimentation.

--- src/main.rs
+++ src/main.rs
@@ -40,28 +40,36 @@ static WAT1: &'static str = r#"
 "#;
 
 
+use std::time::{Duration, SystemTime};
+
+fn bench32() {
+
+}
+
+
 fn main() {
     println!("hey");
+    println!("{:?}", parser::lex("1232 + 23.32/459.4 + 312"))
 
-    let wasm_binary = wat2wasm(WAT).unwrap();
+    // let wasm_binary = wat2wasm(WAT).unwrap();
     
-    let module = compile(&wasm_binary).unwrap();
+    // let module = compile(&wasm_binary).unwrap();
 
-    // We're not importing anything, so make an empty import object.
-    let import_object = imports! {};
+    // // We're not importing anything, so make an empty import object.
+    // let import_object = imports! {};
     
 
-    let instance = module.instantiate(&import_object).unwrap();
+    // let instance = module.instantiate(&import_object).unwrap();
 
-    let add_one: Func<(i32, i32), i32> = instance.func("add").unwrap();
+    // let add_one: Func<(i32, i32), i32> = instance.func("add").unwrap();
 
-    let param: i32 = 41;
-    let value = add_one.call(param, 1);
-    println!("{:?}", value);
+    // let param: i32 = 41;
+    // let value = add_one.call(param, 1);
+    // println!("{:?}", value);
 
-    // assert_eq!(value, 42);
+    // // assert_eq!(value, 42);
      
-    // Ok(())
+    // // Ok(())
 
-    println!("Done");
+    // println!("Done");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,17 +1,28 @@
+extern crate nom;
 
-#[derive(Debug)]
+use std::num::ParseIntError;
+use std::str::FromStr;
+use nom::{
+  IResult,
+  bytes::complete::{tag, take_while_m_n},
+  combinator::map_res,
+  sequence::tuple,
+  character::is_space,
+  character::is_alphanumeric,
+  character::complete::{digit1 as digit, space0 as space},
+  sequence::{delimited, pair},
+  branch::alt,
+  character::complete::char,
+  multi::fold_many0,
+};
+
+#[derive(Debug,PartialEq)]
 enum AbstractNodeType {
-    Compound, 
     Identifier,
-    MemberExpression,
-    Literal, 
-    ThisExpression,
+    Literal,
     CallExpression,
     UnaryExpression,
-    BinaryExpression,
-    LogicalExpression,
-    ConditionalExpression,
-    ArrayExpression
+    BinaryExpression
 }
 
 #[derive(Debug)]
@@ -26,8 +37,7 @@ enum AbstractNode {
     Literal(LiteralNode)
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug,PartialEq)]
 enum LiteralValue {
     BoolVal(bool), 
     FloatVal(f64),
@@ -42,8 +52,8 @@ const SQUOTE_CODE: char = '\'';
 const DQUOTE_CODE: char = '"';
 const OPAREN_CODE: char = '(';
 const CPAREN_CODE: char = ')';
-const OBRACK_CODE: char = '[';
-const CBRACK_CODE: char = ']';
+// const OBRACK_CODE: char = '[';
+// const CBRACK_CODE: char = ']';
 const QUMARK_CODE: char = '?';
 const SEMCOL_CODE: char = ';';
 const COLON_CODE: char = ':';
@@ -53,8 +63,8 @@ const COLON_CODE: char = ':';
 // Worth a micro-benchmark later. 
 const UNARY_OPS: &[&str] = &["-", "NOT", "Not", "not"];
 
-const BINARY_OPS: &[&str] =     &["^", "OR", "Or", "or", "AND", "And", "and", "IS", "Is", "is", "<", ">", "<=", ">=", "+", "-", "*", "/", "%", "MOD", "Mod", "mod"];
-const BINARY_PRECEDENCE: &[i8] = &[1,   1,    1,     1,   2,      2,    2,     6,    6,      6,     7,   7,   7,  7,     9,    9,   10,  10,  10,    10,    10,   10 ];
+const BINARY_OPS: &[&str] =      &["^", "OR", "Or", "or", "AND", "And", "and", "IS", "Is", "is", "<", ">", "<=", ">=", "+", "-", "*", "/"];
+const BINARY_PRECEDENCE: &[i8] = &[1,   1,    1,     1,   2,      2,    2,     6,    6,      6,  7,   7,   7,  7,    9,    9,   10,  10];
 
 // This should be updated any time a longer token is added.
 const MAX_UNARY_LEN: i8 = 3;
@@ -69,190 +79,310 @@ fn throw_error(message: &str, index: i32) {
     println!("{} at character {}", message, index);
 }
 
-// TODO: Create binary expression
-fn is_decimal_digit(ch: char) -> bool {
+fn addToken(token: String, result: &mut Vec<String>){
+    if !token.is_empty() {
+        result.push(token.clone());
+    }
+}
+
+fn is_digit(ch: char) -> bool {
     return ch >= '0' && ch <= '9';
 }
 
-fn is_identifier_start(ch: char) -> bool {
-    return ch == '$' || ch == '_' || 
-    (ch >= 'a' && ch <= 'z') ||
-    (ch >= 'A' && ch <= 'Z');
-    // Jsep also supports any non-ascii char that's not an operator
-    // We'll exclude that since names are separate from IDs
+fn is_alphabetic(ch: char) -> bool {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
 }
 
-fn is_identifier_part(ch: char) -> bool {
-    // Any ascii character and can contain numbers within.
-    return is_identifier_start(ch) || is_decimal_digit(ch);
-}
+pub fn lex(expr: &str) -> Vec<String> {
+    // Split into lexems based on some known operators
+    let mut tokens: Vec<String> = vec![];
+    let mut it = expr.chars().peekable();
 
-fn gobble_spaces(expr: &Vec<char>, start: usize) -> usize {
-    let mut index = start;
-    let len = expr.len();
-    while index < len && expr[index].is_whitespace() {
-        index+=1;
-    }
-    return index;
-}
-
-fn gobble_token(expr: &Vec<char>, start: usize) -> (&str, usize) {
-    let mut index = gobble_spaces(expr, start);
-    let ch: char = expr[index];
-    if is_decimal_digit(ch) || ch == PERIOD_CODE {
-        // TODO
-        println!("{:?}",gobble_numeric_literal(expr, index))
-    }
-    return ("", start); // TODO
-}
-
-fn gobble_digits_helper(expr: &Vec<char>, start: usize) -> (Vec<char>, usize) {
-    let mut index = start;
-    let mut number: Vec<char> = vec![];
-    let length = expr.len();
-    if index >= length {
-        return (number, length);
-    }
-
-    let mut ch = expr[index];
-    while is_decimal_digit(ch) {
-        number.push(ch);
-        index+=1;
-        if index >= length {
-            return (number, length);
+    while let Some(&ch) = it.peek() {
+        // The match should have a case for each starting value of any valid token
+        let mut token = String::from("");
+        match ch {
+            '0'...'9' => {
+                while let Some(&body) = it.peek() {
+                    if is_digit(body) {
+                        token.push(body);
+                        it.next();
+                    }
+                }
+                token.push(ch);
+                it.next();
+            }
+            ' ' => {
+                addToken(token, &mut result);
+                token = String::from("");
+                it.next();
+            }
+            _ => {
+                token.push(ch);
+                it.next();
+            }
         }
-        ch = expr[index];
     }
 
-    return (number, index);
+    return result;
+
 }
 
-// Kind of a very special-case char at, because it will return
-// Empty space if you go out of range. Which differs from jsep, which 
-// can return empty string. Use carefully (i.e. only in comparison against other non space values)
-fn char_at_helper(expr: &Vec<char>, index: usize) -> char {
-    if index < expr.len() {
-        return expr[index];
+// fn is_identifier_start(ch: char) -> bool {
+//     return ch == '$' || ch == '_' || 
+//     (ch >= 'a' && ch <= 'z') ||
+//     (ch >= 'A' && ch <= 'Z');
+//     // Jsep also supports any non-ascii char that's not an operator
+//     // We'll exclude that since names are separate from IDs
+// }
+
+// fn is_identifier_part(ch: char) -> bool {
+//     // Any ascii character and can contain numbers within.
+//     return is_identifier_start(ch) || is_digit(ch);
+// }
+
+
+// Group expressions using parenthesis. Whitespace is optional.
+fn group_expr(s: &str) -> IResult<&str, i64> {
+  delimited(
+    space,
+    delimited(
+      tag("("),
+      expr,
+      tag(")")
+    ),
+    space
+  )(s)
+}
+
+fn factor(i: &str) -> IResult<&str, i64> {
+  alt((
+    map_res(delimited(space, digit, space), FromStr::from_str),
+    group_expr
+  ))(i)
+}
+
+fn term(i: &str) -> IResult<&str, i64> {
+  let (i, init) = factor(i)?;
+
+  fold_many0(
+    pair(alt((char('*'), char('/'))), factor),
+    init,
+    |acc, (op, val): (char, i64)| {
+        if op  == '*' { acc * val } else { acc / val }
     }
-    return ' ';
+  )(i)
 }
 
-fn gobble_numeric_literal(expr: &Vec<char>, start: usize) -> (LiteralNode, usize) {
-    let mut number: Vec<char> = vec![];
-    let mut index = start;
-    let length = expr.len();
-    let mut is_float: bool = false;
+fn expr(i: &str) -> IResult<&str, i64> {
+  let (i, init) = term(i)?;
+
+  fold_many0(
+    pair(alt((char('+'), char('-'))), term),
+    init,
+    |acc, (op, val): (char, i64)| {
+        if op  == '+' { acc + val } else { acc - val }
+    }
+  )(i)
+}
+
+
+fn parse_literal(expr: &Vec<char>, start: usize) -> Option<LiteralNode> {
+    Option::None
+}
+
+// Split
+// +,-,/,^,<=,>=,<,>
+
+
+
+// fn gobble_token(expr: &Vec<char>, start: usize) -> (&str, usize) {
+//     let mut index = gobble_spaces(expr, start);
+//     let ch: char = expr[index];
+//     if is_decimal_digit(ch) || ch == PERIOD_CODE {
+//         // TODO
+//         println!("{:?}",gobble_numeric_literal(expr, index))
+//     }
+//     return ("", start); // TODO
+// }
+
+
+// fn gobble_digits_helper(expr: &Vec<char>, start: usize) -> (Vec<char>, usize) {
+//     let mut index = start;
+//     let mut number: Vec<char> = vec![];
+//     let length = expr.len();
+//     if index >= length {
+//         return (number, length);
+//     }
+//     let mut ch = expr[index];
+//     while is_decimal_digit(ch) {
+//         number.push(ch);
+//         index+=1;
+//         if index >= length {
+//             return (number, length);
+//         }
+//         ch = expr[index];
+//     }
+//     return (number, index);
+// }
+
+// // Kind of a very special-case char at, because it will return
+// // Empty space if you go out of range. Which differs from jsep, which 
+// // can return empty string. Use carefully (i.e. only in comparison against other non space values)
+// fn char_at_helper(expr: &Vec<char>, index: usize) -> char {
+//     if index < expr.len() {
+//         return expr[index];
+//     }
+//     return ' ';
+// }
+
+// fn gobble_numeric_literal(expr: &Vec<char>, start: usize) -> (LiteralNode, usize) {
+//     let mut number: Vec<char> = vec![];
+//     let mut index = start;
+//     let length = expr.len();
+//     let mut is_float: bool = false;
     
-    let (digit, i) = gobble_digits_helper(expr, index);
-    number.extend(digit);
-    index = i;
-    let mut ch = char_at_helper(expr, index);
+//     let (digit, i) = gobble_digits_helper(expr, index);
+//     number.extend(digit);
+//     index = i;
+//     let mut ch = char_at_helper(expr, index);
 
-    if ch == '.'  {
-        number.push(ch);
-        index += 1;
-        is_float = true;
-        let (digit, i) = gobble_digits_helper(expr, index);
-        number.extend(digit);
-        index = i;
-        ch = char_at_helper(expr, index);
-    }
+//     if ch == '.' {
+//         number.push(ch);
+//         index += 1;
+//         is_float = true;
+//         let (digit, i) = gobble_digits_helper(expr, index);
+//         number.extend(digit);
+//         index = i;
+//         ch = char_at_helper(expr, index);
+//     }
 
-    if(ch == 'e' || ch == 'E') { // Exponent marker
-        number.push(ch);
-        index += 1;
-        is_float = true;
-        ch = char_at_helper(expr, index);
-        if(ch == '+' || ch == '-') { // Exponent sign
-            number.push(ch);
-            index += 1;
-        }
-        // Exponent
-        let (digit, i) = gobble_digits_helper(expr, index);
-        number.extend(digit);
-        index = i;
-        if(!is_decimal_digit(char_at_helper(expr, index-1))){
-            // TODO validate
-            // throw_error( &["Expected exponent (", number.iter().collect(), char_at_helper(expr, index), ")"].concat(), index)
-            let num: String = number.iter().collect();
-            println!("Expected exponent ({}{}) at {}", num, char_at_helper(expr, index), index);
-            // TODO - raise error in this case?
-        }
-    }
+//     if(ch == 'e' || ch == 'E') { // Exponent marker
+//         number.push(ch);
+//         index += 1;
+//         is_float = true;
+//         ch = char_at_helper(expr, index);
+//         if(ch == '+' || ch == '-') { // Exponent sign
+//             number.push(ch);
+//             index += 1;
+//         }
+//         // Exponent
+//         let (digit, i) = gobble_digits_helper(expr, index);
+//         number.extend(digit);
+//         index = i;
+//         if(!is_decimal_digit(char_at_helper(expr, index-1))){
+//             // TODO validate
+//             // throw_error( &["Expected exponent (", number.iter().collect(), char_at_helper(expr, index), ")"].concat(), index)
+//             let num: String = number.iter().collect();
+//             println!("Expected exponent ({}{}) at {}", num, char_at_helper(expr, index), index);
+//             // TODO - raise error in this case?
+//         }
+//     }
 
-    let num: String = number.iter().collect();
-    ch = char_at_helper(expr, index);
-    if(is_identifier_start(ch)){
-        println!("Variable names cannot start with a number ({}{}) at {}", num, ch, index)
-    } else if(ch == PERIOD_CODE) {
-        println!("Unexpected period at {}", index)
-    }
+//     let num: String = number.iter().collect();
+//     ch = char_at_helper(expr, index);
+//     if(is_identifier_start(ch)){
+//         println!("Variable names cannot start with a number ({}{}) at {}", num, ch, index)
+//     } else if(ch == PERIOD_CODE) {
+//         println!("Unexpected period at {}", index)
+//     }
 
-    let value: LiteralValue;
+//     let value: LiteralValue;
 
-    if is_float {
-        let v: f64 = num.parse().unwrap();
-        value = LiteralValue::FloatVal(v);
-    } else {
-        let v: i64 = num.parse().unwrap();
-        value = LiteralValue::IntVal(v);
-    }
+//     if is_float {
+//         let v: f64 = num.parse().unwrap();
+//         value = LiteralValue::FloatVal(v);
+//     } else {
+//         let v: i64 = num.parse().unwrap();
+//         value = LiteralValue::IntVal(v);
+//     }
 
-    let node: LiteralNode = LiteralNode { 
-        node_type: AbstractNodeType::Literal, 
-        value: value, 
-        raw: num
-    };
+//     let node: LiteralNode = LiteralNode { 
+//         node_type: AbstractNodeType::Literal, 
+//         value: value, 
+//         raw: num
+//     };
 
-    return (node, index); // TODO
-}
+//     return (node, index); // TODO
+// }
 
-fn gobble_binary_op(expr: &Vec<char>, start: usize) -> (&str, usize) {
-    let mut index = gobble_spaces(expr, start);
-
-
-    // Return operator (or empty string) and continue index.
-    return ("", start); // TODO
-}
-
-fn gobble_binary_expression(expr: &Vec<char>, start: usize) -> usize {
-    return 0; // TODO
-}
-
-pub fn parse(expr: &str) {
-    let mut index = 0;
-    // Char-at isn't constant time due to utf, so do an upfront conversion
-    let expr_vector: Vec<char> = expr.chars().collect();
-    let index = gobble_spaces(&expr_vector, 0);
-    println!("Result index {}", index);
-    gobble_token(&expr_vector, index);
-}
+// pub fn parse(expr: &str) {
+//     let mut index = 0;
+//     // Char-at isn't constant time due to utf, so do an upfront conversion
+//     let expr_vector: Vec<char> = expr.chars().collect();
+//     let index = gobble_spaces(&expr_vector, 0);
+//     println!("Result index {}", index);
+//     gobble_token(&expr_vector, index);
+// }
 
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
+    // #[test]
+    // fn test_parse_int() {
+    //     let expr: Vec<u8> = "42".().collect();
+    //     let (v, i) = gobble_numeric_literal(&expr, 0);
+    //     assert_eq!(v.value, LiteralValue::IntVal(42));
+    // }
+
+    // #[test]
+    // fn test_parse_float() {
+    //     let expr: Vec<char> = "27.9932".chars().collect();
+    //     let (v, i) = gobble_numeric_literal(&expr, 0);
+    //     assert_eq!(v.value, LiteralValue::FloatVal(27.9932));
+    // }
+
+
+    // #[test]
+    // fn test_parse_exponential() {
+    //     let expr: Vec<char> = "27.4e10".chars().collect();
+    //     let (v, i) = gobble_numeric_literal(&expr, 0);
+    //     assert_eq!(v.value, LiteralValue::FloatVal(27.4e10));
+    // }    
+
+
     #[test]
-    fn test_parse_int() {
-        let expr: Vec<char> = "42".chars().collect();
-        let (v, i) = gobble_numeric_literal(&expr, 0);
-        assert_eq!(v.value, LiteralValue::IntVal(42));
+    fn factor_test() {
+        assert_eq!(factor("3"), Ok(("", 3)));
+        assert_eq!(factor(" 12"), Ok(("", 12)));
+        assert_eq!(factor("537  "), Ok(("", 537)));
+        assert_eq!(factor("  24   "), Ok(("", 24)));
     }
 
     #[test]
-    fn test_parse_float() {
-        let expr: Vec<char> = "27.9932".chars().collect();
-        let (v, i) = gobble_numeric_literal(&expr, 0);
-        assert_eq!(v.value, LiteralValue::FloatVal(27.9932));
+    fn term_test() {
+    assert_eq!(term(" 12 *2 /  3"), Ok(("", 8)));
+    assert_eq!(
+        term(" 2* 3  *2 *2 /  3"),
+        Ok(("", 8))
+    );
+    assert_eq!(term(" 48 /  3/2"), Ok(("", 8)));
     }
 
+    #[test]
+    fn expr_test() {
+    assert_eq!(expr(" 1 +  2 "), Ok(("", 3)));
+    assert_eq!(
+        expr(" 12 + 6 - 4+  3"),
+        Ok(("", 17))
+    );
+    assert_eq!(expr(" 1 + 2*3 + 4"), Ok(("", 11)));
+    }
 
     #[test]
-    fn test_parse_exponential() {
-        let expr: Vec<char> = "27.4e10".chars().collect();
-        let (v, i) = gobble_numeric_literal(&expr, 0);
-        assert_eq!(v.value, LiteralValue::FloatVal(27.4e10));
-    }    
+    fn parens_test() {
+    assert_eq!(expr(" (  2 )"), Ok(("", 2)));
+    assert_eq!(
+        expr(" 2* (  3 + 4 ) "),
+        Ok(("", 14))
+    );
+    assert_eq!(
+        expr("  2*2 / ( 5 - 1) + 3"),
+        Ok(("", 4))
+    );
+    }
+
 
 }
