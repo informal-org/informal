c6ae72e4318396c12385322bf5ed10796a031492
Feni Varughese <feni727@gmail.com>
2019-07-10 20:34:24 -0500

    Remove unused templates. Get homepage a rendering and static files a loading.

--- avweb/src/main.rs
+++ avweb/src/main.rs
@@ -1,17 +1,24 @@
 use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{HttpRequest, Result};
 use actix_files as fs;
+use actix_files::NamedFile;
 
 
-fn index(info: web::Path<(u32, String)>) -> impl Responder {
+fn hello(info: web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}", info.1, info.0)
+}
+
+fn index(_req: HttpRequest) -> Result<NamedFile> {
+    Ok(NamedFile::open("templates/index.html")?)
 }
 
 fn main() -> std::io::Result<()> {
     HttpServer::new(
         || App::new().service(
-              web::resource("/{id}/{name}/index.html").to(index))
-              .service(fs::Files::new("/static", "."))           // TODO: Disable this in prod.
+              web::resource("/{id}/{name}/index.html").to(hello))
+              .service(fs::Files::new("/static", "static").show_files_listing())           // TODO: Disable this in prod.
+              .route("/", web::get().to(index))
         )
-        .bind("127.0.0.1:8080")?
+        .bind("localhost:9000")?
         .run()
 }
