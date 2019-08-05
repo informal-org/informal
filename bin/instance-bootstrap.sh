1843173dd9d24a7fffb74b81f770849d061ba5e2
Feni <feni727@gmail.com>
2019-08-04 20:50:52 -0500

    Server instance bootstrap script.

--- bin/instance-bootstrap.sh
+++ bin/instance-bootstrap.sh
@@ -3,3 +3,8 @@ set -ex
 
 # The instance startup script is frozen per group, making changes difficult
 # This script bootstraps an instance and then loads a dynamic installation script which does the actual setup
+RELEASE_URL=$(curl \
+    -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/release-url" \
+    -H "Metadata-Flavor: Google")
+
+gsutil cp gs:// arevel-release.tar.gz
