fa3fa7ba5d447889a8bff848ab65bbd7a841cce0
Feni <feni727@gmail.com>
2019-11-06 17:13:55 -0600

    Re-enable tests for single line expression evaluations.

--- runtime/src/tests.rs
+++ runtime/src/tests.rs
@@ -1,8 +1,16 @@
 
 #[cfg(test)]
 mod tests {
+    use crate::format::repr;
+use crate::interpreter::interpret_expr;
+use crate::parser::apply_operator_precedence;
+use crate::lexer::lex;
+    use avs::constants::APP_SYMBOL_START;
     use super::*;
     use avs::constants::{SYMBOL_TRUE, SYMBOL_FALSE};
+    use avs::environment::Environment;
+    use avs::expression::Expression;
+
     use crate::interpreter;
     use crate::structs::*;
     use serde_json::json;
@@ -15,116 +23,110 @@ mod tests {
     pub use avs::structs::{AvObject};
 
 
-    // fn interpret_one(input: String) -> u64 {
-    //     let mut ast = Environment::new(APP_SYMBOL_START);
-    //     println!("Input: {:?}", input);
-    //     let mut lexed = lex(&mut ast, &input).unwrap();
-    //     println!("Lexed: {:?}", lexed);
-    //     let parsed = parser::apply_operator_precedence(&ast, 0, ast.next_symbol_id, &mut lexed).parsed;
-    //     println!("parsed: {:?}", parsed);
-    //     // TODO: A base, shared global namespace.
-        
-    //     let mut node = Expression::new(0);
-    //     node.parsed = parsed;
-    //     let mut env = Runtime::new(ast.next_symbol_id);
-    //     return interpret_expr(&mut env, &node, &ast);
-    // }
+    fn interpret_one(input: String) -> u64 {
+        let mut env = Environment::new(APP_SYMBOL_START);
+        let mut lexed = lex(&mut env, &input).unwrap();
+        let mut expr = Expression::new(0, input);
+        let parsed = apply_operator_precedence(&mut expr, &mut lexed);
+        // TODO: A base, shared global namespace.
+        return interpret_expr(&mut env, &expr);
+    }
 
 
 
-    // macro_rules! read_eval {
-    //     ($e:expr) => ({
-    //         eval(read(String::from($e)))[0]
-    //     });
-    // }
+    macro_rules! read_eval {
+        ($e:expr) => ({
+            eval(read(String::from($e)))[0]
+        });
+    }
 
-    // macro_rules! read_eval_check {
-    //     ($e:expr, $expected:expr) => ({
-    //         // Execute both a compiled and interpreted version
-    //         let i_result = interpreter::interpret_one(String::from($e));
-    //         let context = Context::new(avs::constants::APP_SYMBOL_START);
+    macro_rules! read_eval_check {
+        ($e:expr, $expected:expr) => ({
+            // Execute both a compiled and interpreted version
+            let i_result = interpret_one(String::from($e));
+            let context = Environment::new(avs::constants::APP_SYMBOL_START);
 
-    //         let fmt_interpreted = format::repr(&Runtime::new(18445899648779484648), &context, i_result);
-    //         let fmt_expected = format::repr(&Runtime::new(18445899648779484648), &context, $expected);
-    //         // TODO: correct avobject
-    //         println!("Checking interpreted result {:?} expected {:?}", fmt_interpreted, fmt_expected);
-    //         assert_eq!(i_result, $expected);
+            let fmt_interpreted = repr(&context, i_result);
+            let fmt_expected = repr(&context, $expected);
+            // TODO: correct avobject
+            println!("Checking interpreted result {:?} expected {:?}", fmt_interpreted, fmt_expected);
+            assert_eq!(i_result, $expected);
             
 
-    //         // let c_result = eval(read(String::from($e)))[0];
-    //         // println!("Checking compiled result");
-    //         // assert_eq!(c_result, $expected);
-    //     });
-    // }
+            // let c_result = eval(read(String::from($e)))[0];
+            // println!("Checking compiled result");
+            // assert_eq!(c_result, $expected);
+        });
+    }
 
-    // macro_rules! read_eval_check_f {
-    //     ($e:expr, $expected:expr) => ({
-    //         // Execute both a compiled and interpreted version
-    //         let i_result = interpreter::interpret_one(String::from($e));
-    //         let i_result_f = f64::from_bits(i_result);
+    macro_rules! read_eval_check_f {
+        ($e:expr, $expected:expr) => ({
+            // Execute both a compiled and interpreted version
+            let i_result = interpret_one(String::from($e));
+            let i_result_f = f64::from_bits(i_result);
 
-    //         println!("Checking interpreted result: {:?} {:?}", i_result_f, $expected);
-    //         assert_eq!(i_result_f, $expected);
+            println!("Checking interpreted result: {:?} {:?}", i_result_f, $expected);
+            assert_eq!(i_result_f, $expected);
             
-    //         // let c_result = eval(read(String::from($e)))[0];
-    //         // let c_result_f = f64::from_bits(c_result);
-    //         // println!("Checking compiled result: {:?} {:?}", c_result, c_result_f);
-    //         // assert_eq!(c_result_f, $expected);
-    //     });
-    // }
+            // let c_result = eval(read(String::from($e)))[0];
+            // let c_result_f = f64::from_bits(c_result);
+            // println!("Checking compiled result: {:?} {:?}", c_result, c_result_f);
+            // assert_eq!(c_result_f, $expected);
+        });
+    }
 
-    // #[test]
-    // fn test_reval_num_literals() {
-    //     read_eval_check_f!("9.0", 9.0);
-    //     read_eval_check_f!("42", 42.0);
-    //     read_eval_check_f!("3.14159", 3.14159);
-    //     read_eval_check_f!("10e5", 10e5);
-    // }
+    #[test]
+    fn test_reval_num_literals() {
+        read_eval_check_f!("9.0", 9.0);
+        read_eval_check_f!("42", 42.0);
+        read_eval_check_f!("3.14159", 3.14159);
+        read_eval_check_f!("10e5", 10e5);
+    }
 
-    // #[test]
-    // fn test_reval_arithmetic() {
-    //     read_eval_check_f!("( 2 ) ", 2.0);
-    //     read_eval_check_f!("1 + 2", 3.0);
-    //     read_eval_check_f!("3 * 2", 6.0);
-    //     read_eval_check_f!("12 * 2 / 3", 8.0);
-    //     read_eval_check_f!("48 / 3 / 2", 8.0);
-    //     read_eval_check_f!("1 + 2 * 3 + 4", 11.0);
-    //     read_eval_check_f!("2 * (3 + 4) ", 14.0);
-    //     read_eval_check_f!("2 * 2 / (5 - 1) + 3", 4.0);
-    // }
+    #[test]
+    fn test_reval_arithmetic() {
+        read_eval_check_f!("( 2 ) ", 2.0);
+        read_eval_check_f!("1 + 2", 3.0);
+        read_eval_check_f!("3 * 2", 6.0);
+        read_eval_check_f!("12 * 2 / 3", 8.0);
+        read_eval_check_f!("48 / 3 / 2", 8.0);
+        read_eval_check_f!("1 + 2 * 3 + 4", 11.0);
+        read_eval_check_f!("2 * (3 + 4) ", 14.0);
+        read_eval_check_f!("2 * 2 / (5 - 1) + 3", 4.0);
+    }
 
-    // #[test]
-    // fn test_unary_minus(){
-    //     read_eval_check_f!("2 + -1", 1.0);
-    //     read_eval_check_f!("5 * -2", -10.0);
-    //     read_eval_check_f!("5 * -(2)", -10.0);
-    //     read_eval_check_f!("5 * -(1 + 1)", -10.0);
-    //     read_eval_check_f!("-(4) + 2", -2.0);
-    // }
+    #[test]
+    fn test_unary_minus(){
+        read_eval_check_f!("2 + -1", 1.0);
+        read_eval_check_f!("5 * -2", -10.0);
+        read_eval_check_f!("5 * -(2)", -10.0);
+        read_eval_check_f!("5 * -(1 + 1)", -10.0);
+        read_eval_check_f!("-(4) + 2", -2.0);
+    }
 
-    // #[test]
-    // fn test_reval_bool() {
-    //     read_eval_check!("true", SYMBOL_TRUE.symbol);
-    //     read_eval_check!("false", SYMBOL_FALSE.symbol);
-    //     read_eval_check!("true or false", SYMBOL_TRUE.symbol);
-    //     read_eval_check!("true and false", SYMBOL_FALSE.symbol);
-    // }
+    #[test]
+    fn test_reval_bool() {
+        read_eval_check!("true", SYMBOL_TRUE.symbol);
+        read_eval_check!("false", SYMBOL_FALSE.symbol);
+        read_eval_check!("true or false", SYMBOL_TRUE.symbol);
+        read_eval_check!("true and false", SYMBOL_FALSE.symbol);
+    }
 
-    // #[test]
-    // fn test_reval_bool_not() {
-    //     // Not is kind of a special case since it's a bit of a unary op
-    //     read_eval_check!("true and not false", SYMBOL_TRUE.symbol);
-    //     read_eval_check!("not true or false", SYMBOL_FALSE.symbol);
-    // }
+    #[test]
+    fn test_reval_bool_not() {
+        // Not is kind of a special case since it's a bit of a unary op
+        read_eval_check!("true and not false", SYMBOL_TRUE.symbol);
+        read_eval_check!("not true or false", SYMBOL_FALSE.symbol);
+    }
 
-    // #[test]
-    // fn test_reval_comparison() {
-    //     read_eval_check!("1 < 2", SYMBOL_TRUE.symbol);
-    //     read_eval_check!("2 < 1", SYMBOL_FALSE.symbol);
-    //     read_eval_check!("2 > 1", SYMBOL_TRUE.symbol);
-    //     read_eval_check!("1 >= 0", SYMBOL_TRUE.symbol);
-    //     read_eval_check!("-1 > 1", SYMBOL_FALSE.symbol);
-    // }
+    #[test]
+    fn test_reval_comparison() {
+        read_eval_check!("1 < 2", SYMBOL_TRUE.symbol);
+        read_eval_check!("2 < 1", SYMBOL_FALSE.symbol);
+        read_eval_check!("2 > 1", SYMBOL_TRUE.symbol);
+        read_eval_check!("1 >= 0", SYMBOL_TRUE.symbol);
+        read_eval_check!("-1 > 1", SYMBOL_FALSE.symbol);
+    }
 
 
     #[test]
