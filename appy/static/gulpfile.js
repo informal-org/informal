49db27359788958ab3f8a2a6f0d6536ddf1e7234
Feni Varughese <feni727@gmail.com>
2020-01-09 21:50:42 -0600

    Fix static files bundling.

--- appy/static/gulpfile.js
+++ appy/static/gulpfile.js
@@ -9,9 +9,6 @@ const concat = require('gulp-concat');
 function build_css() {
   return src('css/*.css')
     .pipe(postcss())
-    .pipe(purgecss({
-        content: ['../templates/**/*.html']
-    }))
     .pipe(minifyCSS())
     .pipe(dest('dist/static/css'))
 }
@@ -21,17 +18,43 @@ function build_sass() {
   .pipe(sass().on('error',sass.logError))
   .pipe(postcss())
   .pipe(dest('dist/static/css'));
+}
+
+function deploy_sass() {
+  src('css/**/*.scss')
+  .pipe(sass().on('error',sass.logError))
+  .pipe(postcss())
+  .pipe(minifyCSS())
+  .pipe(dest('dist/static/css'));
+
+
+  // Treat base separately
+
+  return src('css/base.scss')
+  .pipe(sass().on('error',sass.logError))
+  .pipe(postcss())
+  .pipe(purgecss({
+    content: ['../templates/**/*.html']
+  }))  
+  .pipe(minifyCSS())
+  .pipe(dest('dist/static/css'));
 
 }
 
 function watch_css() {
   watch(['css/*.css'], build_css);  
   watch(['css/*.scss'], build_sass);
+}
 
+function deploy_css(cb) {
+  parallel(deploy_sass, build_css);
+  cb();
 }
 
 // // exports.js = js;
 exports.watch = watch_css;
+
+exports.deploy = deploy_css;
 
 // // exports.default = parallel(css, js);
 // exports.default = parallel(css);
