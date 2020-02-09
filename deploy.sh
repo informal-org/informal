852adc039b6e59a4f5d1868cd6e52076df3c1ed6
Feni Varughese <feni727@gmail.com>
2020-02-08 22:55:50 -0600

    Re-enable restart step for deployment.

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ docker push us.gcr.io/appassembly/aasm:latest
 export deploydate=`date +"%Y-%m-%dT%H_%M"`
 eval "docker tag aasm:latest us.gcr.io/appassembly/aasm:$deploydate"
 eval "docker push us.gcr.io/appassembly/aasm:$deploydate"
-# kubectl rollout restart deployment/aasm-deployment
+kubectl rollout restart deployment/aasm-deployment
