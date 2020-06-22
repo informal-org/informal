c8e0ba3f90d3f4537931a969a049e065afb7b9af
Feni <feni727@gmail.com>
2020-06-22 16:03:56 -0500

    Restructure code generator to support generating more complex code that can't be expressed as inline expressions.

--- appy/static/js/engine/generator.js
+++ appy/static/js/engine/generator.js
@@ -1,10 +1,8 @@
 import { orderCellBody } from "./order"
-import { Obj, KeySignature } from "./flex";
 import { syntaxError } from "./parser";
 
 // TODO: Ensure error equality on CYCLIC_ERR throws error.
 export const JS_PRE_CODE = `
-    
 function ctx_init() {
     var ctx = {};
     return {
@@ -16,7 +14,6 @@ function ctx_init() {
     };
 };
 var ctx = ctx_init();
-
 `;
 // End wrapper function
 export const JS_POST_CODE = `return ctx.all();\n`;
@@ -51,28 +48,49 @@ const BINARY_OPS = {
     "!=": mapToFn("__aa_neq"),
 }
 
-
+// TODO: Vector support for unary minus
 const UNARY_OPS = {
     "-": (left) => { return "-" + left },
     "not": (left) => { return "__aa_not(" + left + ")" }
 }
 
+class CodeGenContext {
+    constructor(env){
+        this.code = JS_PRE_CODE;
+        this.env = env;
+    }
+    add(newCode) {
+        this.code += newCode
+    }
+    finalize() {
+        this.code += JS_POST_CODE;
+    }
+    getCode() {
+        return this.code
+    }
+}
+
 function paramsToJs(node) {
+    // TODO: Preserve normal values as-is for pattern matching
     let params = node.value.map((p) => {
         return "new KeySignature('" + p + "')"
     })
     return params.join(" , ")
 }
 
-function parseGuard(node, params, env) {
+function parseGuard(node, params, code) {
     // TODO: Type support
-    return "(" + params.value + ") => " + astToJs(node, env)
+    return "(" + params.value + ") => " + astToJs(node, code)
 }
 
-function objToJs(node, kv_list, env, name) {
+function objToJs(node, kv_list, code, name) {
+    if(!name) {
+        name = genID();
+    }
+
     let prefix = name ? "var " + name + " = " : "";
-    let code = prefix + "new Obj();";
-            
+    let result = prefix + "new Obj();";
+
     // Array of key value tuples
     kv_list.forEach((kv) => {
         let [k, v] = kv
@@ -86,7 +104,7 @@ function objToJs(node, kv_list, env, name) {
             // TODO: Tuple support for multiple parameters
             // Relies on implicit return in final expression
             // TODO: Return in the context of multiple lines.
-            value = astToJs(v, env) 
+            value = astToJs(v, code) 
 
         } else if(k.node_type == "(grouping)") {
             // It's a parameter. Wrap in an object
@@ -95,7 +113,7 @@ function objToJs(node, kv_list, env, name) {
             // TODO: Type support
             let paramString = paramsToJs(k)
             key = "new KeySignature('', null, [" + paramString + "])"
-            value = "(" + k.value + ") => " + astToJs(v, env)
+            value = "(" + k.value + ") => " + astToJs(v, code)
         } else if(k.node_type == "(where)") {
             // Guard clauses
             // "(: ([ ((grouping) a b) (> a 0)) (+ a b))"
@@ -104,37 +122,35 @@ function objToJs(node, kv_list, env, name) {
             let paramNode = k.left;
             // TODO: Generator support for parameter guards
             let paramString = paramsToJs(paramNode)
-            let guard = parseGuard(k.right, paramNode, env);
+            let guard = parseGuard(k.right, paramNode, code);
             
             key = "new KeySignature('', null, [" + paramString + "],(" + guard + "))"
-            value = "(" + paramNode.value + ") => " + astToJs(v, env)
+            value = "(" + paramNode.value + ") => " + astToJs(v, code)
 
         } else {
             // For flat keys, evaluate both key and value
-            key = astToJs(k, env)
-            value = astToJs(v, env)
+            key = astToJs(k, code)
+            value = astToJs(v, code)
         }
         
-        if(name) {
-            // TODO
-            code += name + ".insert( (" + key + "),(" + value + "));"
-        }
+        result += name + ".insert( (" + key + "),(" + value + "));"
     });
-    return code
+    code.add(result)
+    return name
 }
 
-export function astToJs(node, env, name="") {
+export function astToJs(node, code, name="") {
     // Convert a parsed abstract syntax tree into code
     let prefix = name ? "var " + name + " = " : "";
     if(!node || !node.node_type) { return undefined; }
     switch(node.node_type) {
         case "binary":
-            let left = astToJs(node.left, env);
-            let right = astToJs(node.right, env);
+            let left = astToJs(node.left, code);
+            let right = astToJs(node.right, code);
             // return prefix + "(" + BINARY_OPS[node.operator.keyword](left, right) + ")"
             return prefix + BINARY_OPS[node.operator.keyword](left, right)
         case "unary":
-            return prefix + UNARY_OPS[node.operator.keyword](astToJs(node.left, env))
+            return prefix + UNARY_OPS[node.operator.keyword](astToJs(node.left, code))
         case "(literal)":
             return prefix + JSON.stringify(node.value)
         case "(identifier)":
@@ -143,15 +159,15 @@ export function astToJs(node, env, name="") {
             // let obj = new Obj();
             // let a = {"a": 1, "b": 2}
             // obj.insert(a, "A_VALUE")
-            return objToJs(node, node.value, env, name)
+            return objToJs(node, node.value, code, name)
         }
         case "map": {
-            return objToJs(node, [node.value], env, name)
+            return objToJs(node, [node.value], code, name)
         }
         case "(grouping)": {
             // Top level groupings are expressions
             if(node.value.length == 1) {
-                return "(" + astToJs(node.value[0], env, name) + ")"
+                return "(" + astToJs(node.value[0], code, name) + ")"
             } else {
                 // TODO
                 syntaxError("Unexpected parentheses")
@@ -162,14 +178,14 @@ export function astToJs(node, env, name="") {
             // Left is verified to be an identifier
             let params = [];
             node.value.forEach((param) => {
-                params.push(astToJs(param, env))
+                params.push(astToJs(param, code))
             })
             return prefix + node.left.value + ".call(" + params.join(",") + ")"
         }
         case "(array)": {
             let elems = [];
             node.value.forEach((elem) => {
-                elems.push(astToJs(elem, env))
+                elems.push(astToJs(elem, code))
             })
             return prefix + "Stream.array([" + elems.join(",") + "])"
         }
@@ -179,7 +195,7 @@ export function astToJs(node, env, name="") {
             // }
 
             // TODO: Differentiate between indexing and filtering
-            return prefix + astToJs(node.left, env) + ".get(" + astToJs(node.right, env) + ")"
+            return prefix + astToJs(node.left, code) + ".get(" + astToJs(node.right, code) + ")"
         }
         default:
             console.log("Error: Could not translate ast node: ");
@@ -188,55 +204,50 @@ export function astToJs(node, env, name="") {
     return "";
 }
 
-
-// Bottom up recursive code generation by local evaluation order.
-// (see BURS - bottom up rewrite systems)
-function cellToJs(env, cell) {
-    let code = "";
-
+function cellToJs(code, cell) {
     if(cell.error) {
-        return ""
+        return
     }
 
     if(cell.cyclic_deps) {
         // If it contains cycles, then just report the error.
-        code = `ctx.set("${cell.id}", new CyclicRefError());\n`
+        code.add(`ctx.set("${cell.id}", new CyclicRefError());\n`)
         return code;
     }
 
     // TODO: Variable name check
     var variable_name = cell.name ? cell.name : "__" + cell.id;
-    let expr = astToJs(cell.parsed, env, variable_name);
+    code.add("\ntry { \n");
+    let expr = astToJs(cell.parsed, code, variable_name);
+
     if(expr) {
-        code = "\ntry { \n" + expr + `\nctx.set("${cell.id}", ${variable_name});\n` + "}"
-        code += `catch(err) { console.log(err); ctx.setError("${cell.id}", err.message); }\n`
-    } else if(cell.expr) {  // If it has an expression, but it could not be parsed
-        // TODO
-        // code += `ctx.set("${cell.id}", new ParseError());\n`;
+        code.add(expr)
+        code.add(`\nctx.set("${cell.id}", ${variable_name});\n`)
     }
+    code.add(`} catch(err) { console.log(err); ctx.setError("${cell.id}", err.message); }\n`)
+
 
     
     if(cell.body) {
         let body = orderCellBody(cell);
         body.forEach((child) => {
-            code += cellToJs(env, child);
+            cellToJs(code, child)
         })
     }
     return code
 }
 
 export function genJs(env) {
-    var code = JS_PRE_CODE;
+    var code = new CodeGenContext(env);
 
-    // TODO: Support for nested structures
     if(env.root) {
-        code += cellToJs(env, env.root);
+        cellToJs(code, env.root);
     } else {
         Object.values(env.cell_map).forEach((cell) => {
-            code += cellToJs(env, cell)
+            cellToJs(code, cell)
         })
     }
 
-    code += JS_POST_CODE;
-    return code;
+    code.finalize();
+    return code.getCode();
 }
