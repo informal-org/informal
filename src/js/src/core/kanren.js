ece5c67bcd6560341673a1b54e2c511ec9298127
Feni Varughese <feni727@gmail.com>
2021-03-16 01:12:04 -0500

    Implement generator based 'both' and 'equal' invariants for the mini kanren implementation.

--- src/js/src/core/kanren.js
+++ src/js/src/core/kanren.js
@@ -1,3 +1,5 @@
+// Ported to JS from https://codon.com/hello-declarative-world#unification
+
 class Variable {
     static maxVarId = 0;
     constructor(name) {
@@ -12,6 +14,10 @@ class Variable {
 export class State {
     constructor(variables=[], values = {}) {
         this.variables = variables
+        this.names = {}
+        this.variables.forEach((v) => {
+            this.names[v.name] = v
+        })
         this.values = values
     }
     createVariables(names) {
@@ -29,6 +35,10 @@ export class State {
 
     valueOf(v) {    // Optimization: Avoid recursion here. Use a while loop
         return v instanceof Variable && v.id in this.values ? this.valueOf(this.values[v.id]) : v
+    }
+
+    getByName(name) {
+        return this.names[name]
     }
 
     // Unify variables a and b in this given state.
@@ -50,7 +60,6 @@ export class State {
             ]))
         }
         // Else - cannot unify
-
     }
 
     toString(){
@@ -58,3 +67,99 @@ export class State {
     }
 }
 
+export function* interleave(streamA, streamB) {
+    let aDone, bDone = false;
+    let aVal, bVal;
+
+    while(!aDone || !bDone) {
+        if(!aDone) {
+            aVal = streamA.next();
+            aDone = aVal.done
+            if(!aDone || aVal.value !== undefined) {
+                yield aVal.value
+            }
+        }
+        if(!bDone) {
+            bVal = streamB.next()
+            bDone = bVal.done
+            if(!bDone || bVal.value !== undefined) {
+                yield bVal.value
+            }
+        }
+    }
+}
+
+export class Goal {
+    constructor(cb) {
+        this.cb = cb
+    }
+    pursueIn(state) {
+        return this.cb(state)
+    }
+
+    * pursueInStream(state) {
+        return this.cb(state)
+    }
+
+    * pursueInEach(states) {
+        // States are all of the valid states for the first state.
+        // 
+        let first = states.next();
+
+        if(!first.done) {
+
+            let firstStream = this.pursueIn(first.value)
+            let remainingStreams = this.pursueInEach(states)
+    
+            let combined = interleave(firstStream, remainingStreams);
+            let state;
+            do {
+                state = combined.next()
+                if(!state.done || !state.value !== undefined) {   // hacky way to skip over missing returns
+                    yield state.value
+                }
+            } while(!state.done)
+
+        }
+
+    }
+
+    static equal(a, b) {
+        let f = function*(state) {
+            state = state.unify(a, b)
+            if(state) {
+                yield state
+            }
+        }
+
+        return new Goal(f)
+    }
+
+    static withVariables(names, cb) {
+        let f = function(state) {
+            let variables;
+            [state, variables] = state.createVariables(names);
+            let goal = cb(...variables)
+            return goal.pursueIn(state)
+        }
+        return new Goal(f)
+    }
+
+    static either(firstGoal, secondGoal) {
+        let f = function(state) {
+            let firstStream = firstGoal.pursueIn(state)
+            let secondStream = secondGoal.pursueIn(state)
+            return interleave(firstStream, secondStream)
+        }
+
+        return new Goal(f)
+    }
+
+    static both(firstGoal, secondGoal) {
+        let f = function(state) {
+            let states = firstGoal.pursueIn(state)
+            return secondGoal.pursueInEach(states);
+        }
+        return new Goal(f)
+    }
+}
--- src/js/tests/core/kanren.test.js
+++ src/js/tests/core/kanren.test.js
@@ -1,4 +1,4 @@
-import { State } from "@informal/core/kanren.js"
+import { State, Goal, interleave } from "@informal/core/kanren.js"
 
 test('Test unify', () => {
     let state = new State();
@@ -10,4 +10,97 @@ test('Test unify', () => {
     state = state.unify(x, y)
     state = state.unify(x, 5)
     expect(state.valueOf(x)).toEqual(5)
+});
+
+test('Test equal goal', () => {
+    let state = new State();
+    let x, y, z;
+    [state, [x, y, z]] = state.createVariables(["x", "y", "z"])
+
+    let goal = Goal.equal(x, 5)
+    let states = goal.pursueIn(state);
+
+    let goalState = states.next().value;
+    expect(goalState.valueOf(x)).toEqual(5);
+});
+
+test('Test goal with variables', () => {
+    let goal = Goal.withVariables(["x"], (x) => Goal.equal(x, 5))
+    let states = goal.pursueIn(new State());
+    let state = states.next().value;
+    expect(state.valueOf(state.getByName("x"))).toEqual(5);
+})
+
+test('Test stream interleave', () => {
+    let letters = function*() {
+        while(true){
+            yield "A"
+            yield "B"
+            yield "C"
+        }
+    }
+
+    let numbers = function*() {
+        for(var i = 0; i < 5; i++) {
+            yield i
+        }
+    }
+
+    let combined = interleave(letters(), numbers());
+    let combinedValues = [];
+    for(var i = 0; i < 18; i++) {
+        combinedValues.push(combined.next().value)
+    }
+    expect(combinedValues).toEqual(["A", 0, "B", 1, "C", 2,
+        "A", 3, "B", 4, "C", "A",
+        "B", "C", "A", "B", "C", "A",
+    ]);
+})
+
+test('Test either goal', () => {
+    let goal = Goal.withVariables(["x"], (x) => {
+        return Goal.either(
+            Goal.equal(x, 5),
+            Goal.equal(x, 6),
+        )
+    });
+    let states = goal.pursueIn(new State());
+    let state = states.next().value;
+    expect(state.valueOf(state.getByName("x"))).toEqual(5)
+    state = states.next().value;
+    expect(state.valueOf(state.getByName("x"))).toEqual(6)
+})
+
+
+test('Test both goal', () => {
+    let goal = Goal.withVariables(["a", "b"], (a, b) => {
+        return Goal.both(
+            Goal.equal(a, 7),
+            Goal.either(
+                Goal.equal(b, 5),
+                Goal.equal(b, 6)
+            )
+        )
+    });
+    let states = goal.pursueIn(new State());
+    let state = states.next().value;
+    expect(state.valueOf(state.getByName("a"))).toEqual(7)
+    expect(state.valueOf(state.getByName("b"))).toEqual(5)
+    state = states.next().value;
+    expect(state.valueOf(state.getByName("a"))).toEqual(7)
+    expect(state.valueOf(state.getByName("b"))).toEqual(6)
+})
+
+test('Test incompatible goals', () => {
+    let goal = Goal.withVariables(["x"], (x) => {
+        return Goal.both(
+            Goal.equal(1, x),
+            Goal.equal(x, 2)
+        )
+    })
+    let states = goal.pursueIn(new State());
+    let state = states.next()       // Yields undefined
+    expect(state.value).toEqual(undefined)
+    state = states.next()           // Function returns (nothing)
+    expect(state.done).toEqual(true)
 });
