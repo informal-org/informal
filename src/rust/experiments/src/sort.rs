efb33f9123d8e7bc5f520c58932558902705040a
Feni Varughese <feni727@gmail.com>
2021-04-01 23:54:13 -0500

    Tried out a heap sort implementation. Doesn't perform nearly as well as either std sort or the graph sort.

--- src/rust/experiments/src/sort.rs
+++ src/rust/experiments/src/sort.rs
@@ -56,10 +56,19 @@ fn graph_sort(list: &[u64]) -> Vec<u64> {
         // println!("Pivot idx {:?} of {:?}", pivotIdx, pivots.len());
         if pivotIdx == pivots.len() {
             // println!("Inserting new arr");
-            let mut subVec: Vec<u64> = Vec::new();
-            subVec.push(*elem);
-            pivots.push(subVec)
-            
+            // Optimization: Prepend (incurring the array shift cost) onto the previous pivot if the array is small.
+            if pivots.len() > 0 && pivots.last().unwrap().len() <= 8 {
+                // Insert the previous pivot into the proper place in the vector
+                let previousPivot = pivots.last().unwrap()[0];
+                binary_insert(&mut pivots[pivotIdx - 1], previousPivot);
+                pivots.last_mut().unwrap()[0] = *elem;   // Set the new pivot
+            } else {
+                // Create a new pivot if it's too expensive to shift the old one.
+                let mut subVec: Vec<u64> = Vec::with_capacity(16);
+                subVec.push(*elem);
+                pivots.push(subVec)                
+            }
+
         } else {
             // println!("Binary inserting into arr");
             binary_insert(&mut pivots[pivotIdx], *elem);
@@ -82,6 +91,138 @@ fn graph_sort(list: &[u64]) -> Vec<u64> {
     }
     return sorted;
 }
+
+const HEAP_LEN: usize = 16;
+
+struct Heap {
+    pivot: u64,
+    left_arr: Vec<u64>,
+    left_heap: Box<Option<Heap>>,
+
+    right_arr: Vec<u64>,
+    right_heap: Box<Option<Heap>>
+}
+
+fn init_heap(pivot: u64) -> Heap {
+    return Heap { 
+        pivot: pivot,
+        left_arr: Vec::with_capacity(HEAP_LEN),
+        left_heap: Box::new(Option::None),
+        right_arr: Vec::with_capacity(HEAP_LEN),
+        right_heap: Box::new(Option::None)
+    }
+}
+
+
+fn heap_insert(heap: &mut Heap, element: u64) {
+    if element <= heap.pivot {
+        // If the array isn't full, insert into it.
+        // if heap.left_arr.len() < HEAP_LEN {
+        //     let idx = heap.left_arr.binary_search(&element).unwrap_or_else(|x| x);
+        //     heap.left_arr.insert(idx, element);
+        // } else if heap.left_heap.is_none() {
+        //     heap.left_heap = Box::new(Option::Some(init_heap(element)));
+        // } else {
+        //     heap_insert(&heap.left_heap.unwrap(), element)
+        // }
+
+        match heap.left_heap.as_mut() {
+            Option::None => {
+                if heap.left_arr.len() < HEAP_LEN {
+                    let idx = heap.left_arr.binary_search(&element).unwrap_or_else(|x| x);
+                    heap.left_arr.insert(idx, element);
+                } else {    // Time to split the heap
+                    // The midpoint is likely to be a good pivot since this array is already sorted.
+                    let midpoint = heap.left_arr.len() / 2;
+                    let mut left_heap = init_heap(heap.left_arr[midpoint]);
+                    left_heap.left_arr.extend_from_slice(&heap.left_arr[0..midpoint]);
+                    left_heap.left_arr.extend_from_slice(&heap.left_arr[midpoint + 1..]);
+    
+                    heap_insert(&mut left_heap, element);
+                    heap.left_heap = Box::new(Option::Some(left_heap));
+                }
+            },
+            Option::Some(left) => {
+                heap_insert(left, element);
+            }
+        }
+    } else {
+        // Right
+
+        match heap.right_heap.as_mut() {
+            Option::None => {
+                if heap.right_arr.len() < HEAP_LEN {
+                    let idx = heap.right_arr.binary_search(&element).unwrap_or_else(|x| x);
+                    heap.right_arr.insert(idx, element);
+                } else {    // Time to split the heap
+                    // The midpoint is likely to be a good pivot since this array is already sorted.
+                    let midpoint = heap.right_arr.len() / 2;
+                    let mut right_heap = init_heap(heap.right_arr[midpoint]);
+                    right_heap.left_arr.extend_from_slice(&heap.right_arr[0..midpoint]);
+                    right_heap.right_arr.extend_from_slice(&heap.right_arr[midpoint + 1..]);
+
+                    heap_insert(&mut right_heap, element);
+                    heap.right_heap = Box::new(Option::Some(right_heap));
+                }
+
+            },
+            Option::Some(right) => {
+                heap_insert(right, element)
+            }
+        }
+
+
+        // if heap.right_heap.is_none() {
+        //     if heap.right_arr.len() < HEAP_LEN {
+        //         let idx = heap.right_arr.binary_search(&element).unwrap_or_else(|x| x);
+        //         heap.right_arr.insert(idx, element);
+        //     } else {    // Time to split the heap
+        //         // The midpoint is likely to be a good pivot since this array is already sorted.
+        //         let midpoint = heap.right_arr.len() / 2;
+        //         let mut right_heap = init_heap(heap.right_arr[midpoint]);
+        //         right_heap.left_arr.extend_from_slice(&heap.right_arr[0..midpoint]);
+        //         right_heap.right_arr.extend_from_slice(&heap.right_arr[midpoint + 1..]);
+
+        //         heap_insert(&mut right_heap, element);
+        //         heap.right_heap = Box::new(Option::Some(right_heap));
+        //     }
+        // } else {
+        //     heap_insert(&mut heap.right_heap.unwrap(), element)
+        // }
+    }
+}
+
+fn copy_to_array(heap: &Heap, vec: &mut Vec<u64>) {
+    // if heap.left_heap.is_none() {
+    //     vec.extend_from_slice(&heap.left_arr);
+    // } else {
+    //     copy_to_array(&heap.left_heap.unwrap(), vec);
+    // }
+
+    match heap.left_heap.as_ref() {
+        Option::None => vec.extend_from_slice(&heap.left_arr),
+        Option::Some(left) => copy_to_array(&left, vec)
+    }
+
+
+    vec.push(heap.pivot);
+
+    match heap.right_heap.as_ref() {
+        Option::None => vec.extend_from_slice(&heap.right_arr),
+        Option::Some(right) => copy_to_array(&right, vec)
+    }
+}
+
+fn heap_array_sort(list: &[u64]) -> Vec<u64> {
+    let mut heap = init_heap(list[0]);
+    for elem in list {
+        heap_insert(&mut heap, *elem);
+    }
+    let mut sorted: Vec<u64> = Vec::with_capacity(list.len());
+    copy_to_array(&heap, &mut sorted);
+    return sorted;
+}
+
 
 mod tests {
     use super::*;
@@ -118,8 +259,18 @@ mod tests {
             let result = graph_sort(&arr[..]);
             // println!("Sorted? {:?}", result);
         });
-        
     }
+
+
+    #[bench]
+    fn bench_heap_array_sort(b: &mut Bencher) {
+        // println!("Testing gensort");
+        b.iter(|| {
+            let mut arr = gensort(BENCH_SIZE);
+            let result = heap_array_sort(&arr[..]);
+            // println!("Sorted? {:?}", result);
+        });
+    }    
 
 }
 
@@ -147,4 +298,15 @@ test sort::tests::bench_std_sort   ... bench:   4,188,829 ns/iter (+/- 67,575)
 It's an nlog(n) sort with a constant factor slowdown of 3 vs timsort.
 I suspect all of the sub-allocations for the sub-arrays are causing most of the slowdown.
 There are some approaches I can take to counteract that, but may not be worth the effort.
+
+Tried to fix the problem of small buckets with a few elements by having it merge with the previous
+bucket on insert in some cases, giving a min bucket size.
+
+Did not make a big different. (Max bucket size of 8. Seemed to be the sweet spot)
+
+test sort::tests::bench_graph_sort ... bench:     906,551 ns/iter (+/- 35,091)
+test sort::tests::bench_std_sort   ... bench:     345,090 ns/iter (+/- 1,171)
+
+
+
 */
