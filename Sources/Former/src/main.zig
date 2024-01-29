c0323b4ac37a95a708ad4a9329d14eb2ed31f554
Feni <feni727@gmail.com>
2024-01-28 22:55:17 -0600

    Theoretical table-based float parser.

--- Sources/Former/src/main.zig
+++ Sources/Former/src/main.zig
@@ -1,21 +1,25 @@
 const std = @import("std");
-const Parser = @import("parser.zig").Parser;
-const wasm = @import("wasm.zig");
+// const Parser = @import("parser.zig").Parser;
+// const wasm = @import("wasm.zig");
+const tblparser = @import("tableparser.zig");
+
 
 pub fn main() !void {
     // const arena_allocator = std.heap.ArenaAllocator;
-    var buffer: [10000]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    const allocator = fba.allocator();
+    // var buffer: [10000]u8 = undefined;
+    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    // const allocator = fba.allocator();
+    //
+    // const stdin = std.io.getStdIn().reader();
+    // const value = try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 100);
+    // defer allocator.free(value.?);
+    //
+    // var parser = Parser.init(value.?, allocator);
+    // // var parser = Parser.init("1 + 1", allocator);
+    // defer parser.deinit();
+    //
+    // try parser.lex();
+    // try wasm.emit(&parser);
 
-    const stdin = std.io.getStdIn().reader();
-    const value = try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 100);
-    defer allocator.free(value.?);
 
-    var parser = Parser.init(value.?, allocator);
-    // var parser = Parser.init("1 + 1", allocator);
-    defer parser.deinit();
-
-    try parser.lex();
-    try wasm.emit(&parser);
 }
--- Sources/Former/src/tableparser.zig
+++ Sources/Former/src/tableparser.zig
@@ -1,10 +1,14 @@
+const std = @import("std");
+
+const Allocator = std.mem.Allocator;
 
 
 const ParseOp = enum {
     push,  // Push current char to context stack.
     pop,   // Emit the top of the current context stack.
-    output, // Emit current character to output.
     fail,   // Terminate with an error.
+    emit,   // At an unambiguous terminal. Emit the current type.
+    advance, // Advance the input, building up the current token.
     skip,   // Advance the input without emitting anything.
     peek    // Don't output or advance. Transition to next state and peek at current context rather than input.
 };
@@ -24,7 +28,8 @@ const ParseSubState = struct {
 
 const StateTransition = struct {
     operation: ParseOp,
-    next: u32
+    next: u32,
+    type: u8              //
 };
 
 const CompiledParseState = struct {
@@ -35,6 +40,54 @@ const CompiledParseState = struct {
     next: *[u8]StateTransition, // The other possible next-states. Indexed by popcnt-index.
     fail: StateTransition, // What to do if the match failed.
 };
+
+const Token = struct {
+    type: u8,
+    start: u32,
+    end: u32,
+};
+
+
+pub const TableParser = struct {
+    const Self = @This();
+    states: []const ParseSubState,
+    //compiled: []const CompiledParseState,
+
+    allocator: Allocator,
+
+    pub fn init(states: []const ParseSubState, allocator: Allocator) Self {
+        return Self { .states = states, .allocator = allocator };
+    }
+
+    // pub fn compile(self: *Self) !void {
+    //
+    // }
+
+};
+
+
+const test_allocator = std.testing.allocator;
+const expect = std.testing.expect;
+const print = std.debug.print;
+
+test "Float parser" {
+    // Simplified float parser. No support for e.
+    const tbl = [_]ParseSubState{
+        ParseSubState{ .id = 0, .match = "0123456789", .operation = ParseOp.advance, .next = 1 },
+        ParseSubState{ .id = 0, .match = ".", .operation = ParseOp.advance, .next = 2 },
+        // ParseSubState{ .id = 0, .match = SENTINEL_END, .operation = ParseOp.fail, .next = 0 },  // Empty string.
+        ParseSubState{ .id = 1, .match = "0123456789", .operation = ParseOp.advance, .next = 1 },
+        ParseSubState{ .id = 1, .match = ".", .operation = ParseOp.advance, .next = 2 },
+        ParseSubState{ .id = 1, .match = SENTINEL_END, .operation = ParseOp.emit, .next = 0 },
+        ParseSubState{ .id = 2, .match = "0123456789", .operation = ParseOp.advance, .next = 3 },
+        // ParseSubState{ .id = 2, .match = SENTINEL_END, .operation = ParseOp.fail, .next = 0 },  // . without fractions.
+        ParseSubState{ .id = 3, .match = "0123456789", .operation = ParseOp.advance, .next = 3 }, // Parse fractions.
+        ParseSubState{ .id = 3, .match = SENTINEL_END, .operation = ParseOp.emit, .next = 0 },
+    };
+    const parser = TableParser.init(tbl, test_allocator);
+    expect(parser.states.len == 10);
+}
+
 
 
 // id, match, operation, next
