1f8a488aa3d211c4e9ad44ff5e6acc4a3a4c7da2
Feni <feni727@gmail.com>
2024-02-10 20:15:11 -0600

    Array tests.

--- Sources/Former/tests/compiler/script.test.js
+++ Sources/Former/tests/compiler/script.test.js
@@ -2,18 +2,27 @@ import { evaluate } from "../../src"
 var dedent = require('dedent-js');
 
 
-// test('Evaluate arithmetic scripts', () => {
-//     expect(evalScript("1 + 2 * 3 + 4 / 2 - 4")).toEqual(5);
-// })
+test('Evaluate arithmetic scripts', () => {
+    expect(evalScript("1 + 2 * 3 + 4 / 2 - 4")).toEqual(5);
+})
 
 test('Evaluate multi-line expression', () => {
     expect(evalScript(dedent`
     a = 1
     b = 2
     a + b
-`)).toEqual(3);
+    `)).toEqual(3);
+})
 
-    // expect(evalScript("():\na = 1\nb = 2\na + b")).toEqual(3);
+test('Eval array expressions', () => {
+    const result = evalScript(dedent`
+    a = [1, 2, 3]
+    b = a + 2
+    c = a * 2
+    b + c
+`);
+    const resultArray = Array.from(result.iter());
+    expect(resultArray).toEqual([5, 8, 11]); // [3, 4, 5] + [2, 4, 6]
 })
 
 
